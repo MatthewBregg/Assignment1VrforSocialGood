ab88f4b5021b0362d6e8ecb0d83df07de5b4ade2
GAMES <reidgill20@gmail.com>
2018-02-04 18:46:34 -0800

    Fixed human avatar issue

---
+++ Assets/CopyRotationFromCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyRotationFromCamera : MonoBehaviour {
+    public Camera mainCamera;
+    // Use this for initialization
+
+    public Vector3 transformOffset;
+	void Start () {
+	}
+	
+	
+	void FixedUpdate () {
+        float cameraY = mainCamera.transform.rotation.eulerAngles.y;
+        Vector3 myRotation = transform.rotation.eulerAngles;
+        myRotation.y = cameraY;
+
+        transform.rotation = Quaternion.Euler(myRotation);
+
+        transform.position = mainCamera.transform.position + transformOffset;
+
+        
+	}
+}
