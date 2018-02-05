fd8f680b6847950a588c29995dfa8388709140ac
GAMES <reidgill20@gmail.com>
2018-02-04 23:00:07 -0800

    Added script to split/combine the piles!

---
+++ Assets/SplitOrCombineActionOnEnter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitOrCombineActionOnEnter : TriggerIfPlayerEntersAction {
+    public GameObject combinedPile;
+    public GameObject splitPile;
+    public override void performEnterAction(Collider platform, Collider other) {
+       if ( splitPile.activeSelf ) {
+            splitPile.SetActive(false);
+            combinedPile.SetActive(true);
+        } else {
+            splitPile.SetActive(true);
+            combinedPile.SetActive(false);
+        }
+    }
+
+    public override void performExitAction(Collider platform, Collider other) {
+     
+    }
+
+}
