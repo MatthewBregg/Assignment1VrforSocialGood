17ab3ffac0796d9efab39d98395994ad78658361
GAMES <reidgill20@gmail.com>
2018-02-07 20:03:06 -0800

    Added audio tutorials

---
+++ Assets/EnableNextSignAndPlayTutorialAudioOnceWhenSteppedOn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableNextSignAndPlayTutorialAudioOnceWhenSteppedOn : TriggerIfPlayerEntersAction {
+    public AudioClip startingClip = null;
+    public AudioSource src;
+
+    public GameObject enableNext = null;
+
+    private bool playedAlready = false;
+
+    public override void performEnterAction(Collider platform, Collider other) {
+        if (enableNext != null) {
+            enableNext.SetActive(true);
+        }
+    }
+
+    public override void performExitAction(Collider platform, Collider other) {
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (startingClip != null) {
+            src.Stop();
+            src.PlayOneShot(startingClip);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PlaySoundOnAwakeFromExistingAudioSource.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnAwakeFromExistingAudioSource : MonoBehaviour {
+
+    public AudioClip toPlay;
+    public AudioSource src;
+
+	// Use this for initialization
+	void Start () {
+        src.Stop();
+        src.PlayOneShot(toPlay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
